49180fb3097fc3eee2c6645624e6505c146da5a6
unknown <uwampojejecharly@gmail.com>
2024-02-19 10:34:36 +0200

    Create a library
    Reconfigurated a Java library executable with or without maven.

---
+++ src/main/java/fr/charly/mavenTraining/Hello.java
@@ -0,0 +1,9 @@
+package fr.charly.mavenTraining;
+
+public class Hello {
+	
+	public void printGreetings(String name,String firstName) {
+		System.out.println(" Hello " + firstName + " " + name);
+	}
+
+}
